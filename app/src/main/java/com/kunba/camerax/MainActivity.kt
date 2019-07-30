26915e71d30ad2dd946386cfb4296c8219216c42
Fatih <kunbaser@gmail.com>
2019-07-30 22:30:30 +0200

    first commit

---
+++ app/src/main/java/com/kunba/camerax/MainActivity.kt
@@ -0,0 +1,145 @@
+package com.kunba.camerax
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.os.Bundle
+import android.util.Rational
+import android.view.Surface
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.nio.ByteBuffer
+
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewFinder = findViewById(R.id.view_finder)
+
+        if (allPermissionsGranted()) {
+            viewFinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+    }
+
+    private lateinit var viewFinder: TextureView
+
+    private fun startCamera() {
+
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetAspectRatio(Rational(1, 1))
+            setLensFacing(CameraX.LensFacing.BACK)
+        }.build()
+
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .apply {
+                setTargetAspectRatio(Rational(1, 1))
+                setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+            }.build()
+
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        btn_take_photo.setOnClickListener {
+
+            imageCapture.takePicture(
+                object : ImageCapture.OnImageCapturedListener() {
+                    override fun onCaptureSuccess(image: ImageProxy?, rotationDegrees: Int) {
+                        super.onCaptureSuccess(image, rotationDegrees)
+
+                        Toast.makeText(applicationContext, "Image captured", Toast.LENGTH_LONG).show()
+
+                        //handle image here -> use imageProxyToBitmap() and transform using Glide
+
+                    }
+                })
+        }
+        CameraX.bindToLifecycle(this, preview, imageCapture)
+
+    }
+
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        val rotationDegrees = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        viewFinder.setTransform(matrix)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun imageProxyToBitmap(image: ImageProxy): Bitmap {
+        val buffer: ByteBuffer = image.planes[0].buffer
+        val bytes = ByteArray(buffer.remaining())
+        buffer.get(bytes)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
+
+}
+
